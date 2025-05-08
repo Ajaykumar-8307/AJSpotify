7dcfe9003845236a6fe4f598b088fbc9bffe1700
Ajaykumar-8307 <153371277+Ajaykumar-8307@users.noreply.github.com>
2025-05-08 22:10:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+// Load the built-in 'http' module
+const http = require('http');
+
+// Define a hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create the server
+const server = http.createServer((req, res) => {
+  // Set the response HTTP header
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write('Vanakkam');
+  res.end('Hello, World!\n');
+});
+
+// Start listening on the port
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
